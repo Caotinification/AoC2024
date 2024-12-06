4bcea5085b1664ae7d975aa792a42dc7582d5924
Caotinification <nguyenjustin930@gmail.com>
2024-12-06 16:37:47 -0500

    Day1 is solved for both parts.

--- src/Days/Day1.scala
+++ src/Days/Day1.scala
@@ -2,11 +2,17 @@ package org.caotin
 package Days
 
 // 2430334
-object Day1 extends Solution[Int]:
-  override def solve(input: Iterator[String]): Int = {
+object Day1 extends Solution[(Int, Int)]:
+  override def solve(input: Iterator[String]): (Int, Int) = {
+    //part 1
     val pairs = input.map(str => str.split(" {3}")).toVector
     val leftList = pairs.map(array => array(0).toInt).sorted
     val rightList = pairs.map(array => array(1).toInt).sorted
     val distances = leftList.zip(rightList).map((l,r) => Math.abs(l - r))
-    distances.sum
+
+    //part 2
+    val elementCounts = leftList.map(x => x -> rightList.count(_ == x))
+    val simScore = elementCounts.map((element, count) => element * count).sum
+
+    (distances.sum, simScore)
   }
--- src/Main.scala
+++ src/Main.scala
@@ -5,5 +5,6 @@ import Days.Day1
 @main def main(): Unit = {
   val source = scala.io.Source.fromFile("Inputs/day1.txt")
   val lines = source.getLines()
-
+  println(Day1.solve(lines))
+  source.close()
 }
